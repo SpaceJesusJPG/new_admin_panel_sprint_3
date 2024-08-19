0f52141a3be17c139d400b5b0517280db952f697
Никита Гладышев <ja_ne_smog_pridumat@mail.ru>
2024-08-20 00:21:57 +1200

    very close, but no really

---
+++ postgres_to_es/utilities/backoff.py
@@ -0,0 +1,32 @@
+import time
+from functools import wraps
+
+
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+
+    Формула:
+        t = start_sleep_time * (factor ^ n), если t < border_sleep_time
+        t = border_sleep_time, иначе
+    :param start_sleep_time: начальное время ожидания
+    :param factor: во сколько раз нужно увеличивать время ожидания на каждой итерации
+    :param border_sleep_time: максимальное время ожидания
+    :return: результат выполнения функции
+    """
+
+    def func_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            sleep_time = start_sleep_time
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"Ошибка при выполнении {func.__name__}: {e}")
+                    time.sleep(sleep_time)
+                    sleep_time = min(sleep_time * factor, border_sleep_time)
+
+        return inner
+
+    return func_wrapper
